76218971302a7ae2d52a739abeb8d0afeefa7750
mhmdfrhn <frhn2707@gmail.com>
2022-12-06 21:59:29 +0700

    create group routes

---
+++ app/(users)/todos/head.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Head() {
+  return (
+    <>
+      <title>Todos page</title>
+    </>
+  );
+}
+
+export default Head;
--- app/(users)/todos/page.tsx
+++ app/(users)/todos/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import TodoList from "./TodoList";
 
 function Todos() {
   return (
     <div>
-      {/* @ts-ignore */}
-      <TodoList />
+      <h1>This is where the Todos will be listed...</h1>
     </div>
   );
 }
